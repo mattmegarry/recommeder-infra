b67f5a966e5e52cb4f0c5e79d98ee64e2dbbddf9
Matt Megarry <mattmegarry@gmail.com>
2023-07-23 18:34:31 +0100

    recommendation data is saved to kafka topic: logs upon user receiving recommendation via home route

--- api/main.py
+++ api/main.py
@@ -1,6 +1,8 @@
 import fastapi
 import time
 import datetime
+import json
+import confluent_kafka
 
 from .postgres.connection import get_postgres_connection
 from .postgres.queries import getOneSemiRandomItemKey, create_fct_metric
@@ -10,16 +12,23 @@ app = fastapi.FastAPI()
 @app.on_event("startup")
 async def startup_event():
   app.state.connection = get_postgres_connection()
+  app.state.k = confluent_kafka.Producer({"bootstrap.servers": "kafka:29092"})
   print("FastAPI app has started")
 
 @app.get("/")
 def read_root(request: fastapi.Request):
+  ts = int(time.time())
   user_id = request.headers.get("user")
   session = request.headers.get("session")
 
   print(f"User: {user_id} Session: {session}")
 
   item_key = getOneSemiRandomItemKey(app)
+
+  log_msg = json.dumps({"type": "reco", "user_id": user_id, "session": session, "item_id": item_key, "ts": ts})
+  app.state.k.produce("logs", log_msg)
+  if (len(app.state.k) > 5): app.state.k.flush()
+
   return item_key
 
 @app.post("/evt")

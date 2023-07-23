e8940c16c45c510ec28ef14665f42a0f783ba8a8
Matt Megarry <mattmegarry@gmail.com>
2023-07-23 23:03:44 +0100

    in / reco data is saved to redis AND in /evt that reco data is extracted from redis to give kafta topic: *logs* evt data an item_id - kafka_to_pg_logs worker is thus operational

--- api/main.py
+++ api/main.py
@@ -1,11 +1,14 @@
 import fastapi
 import time
-import datetime
 import json
 import confluent_kafka
+import os
+import redis
 
 from .postgres.connection import get_postgres_connection
 from .postgres.queries import get_one_semi_random_item_key, create_fct_metric
+
+REDIS_PASSWORD = os.environ.get('REDIS_PASSWORD')
 
 app = fastapi.FastAPI()
 
@@ -13,6 +16,7 @@ app = fastapi.FastAPI()
 async def startup_event():
   app.state.connection = get_postgres_connection()
   app.state.k = confluent_kafka.Producer({"bootstrap.servers": "kafka:29092"})
+  app.state.r = redis.Redis(host="redis", port=6379, db=0, password=REDIS_PASSWORD, encoding='utf-8')
   print("FastAPI app has started")
 
 @app.get("/")
@@ -24,6 +28,9 @@ def read_root(request: fastapi.Request):
   print(f"User: {user_id} Session: {session}")
 
   item_key = get_one_semi_random_item_key(app)
+
+  reco_info = {"item_id": item_key, "ts": ts}
+  app.state.r.xadd(f"x:{user_id}:{session}", reco_info, maxlen=30, approximate=True)
 
   log_msg = json.dumps({"type": "reco", "user_id": user_id, "session": session, "item_id": item_key, "ts": ts})
   app.state.k.produce("logs", log_msg)
@@ -42,8 +49,20 @@ def save_event(request: fastapi.Request):
   event = request.headers.get("event")
   ######-----------------------#######
 
-  log_msg = json.dumps({"type": "evt", "user_id": user_id, "session": session, "ts": ts})
-  app.state.k.produce("logs", log_msg)
-  app.state.k.flush()
+  redis_key = f"x:{user_id}:{session}" 
+  most_recent_record = app.state.r.xrevrange(redis_key, count=1)
+
+  if len(most_recent_record) != 0:
+    # pedagogical...
+    most_recent_tuple = most_recent_record[0]
+    most_recent_reco_dict = most_recent_tuple[1]
+    most_recent_reco_item_id = most_recent_reco_dict[b"item_id"].decode("utf-8")
+
+    log_msg = json.dumps({"type": "evt", "user_id": user_id, "session": session, "item_id": most_recent_reco_item_id, "ts": ts})
+    app.state.k.produce("logs", log_msg)
+    app.state.k.flush()
+    print("Event saved")
+  else:
+    print("TODO: What happens if we dont know about that recommendation?")
 
   return "ok"
